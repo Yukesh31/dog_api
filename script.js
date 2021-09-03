f402dfe6b95ad61f3ab0bb30df3692e04279fde2
Yukesh31 <vyukeshkumaran@gmail.com>
2021-09-03 20:28:34 +0530

    second

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+let cat = document.getElementById('cat');
+let btn_cat = document.getElementById('btn_cat');
+
+btn_cat.addEventListener('click', get_random_cat);
+
+async function get_random_cat() {
+  let a = await fetch('https://random.dog/woof.json');
+  let b = await a.json();
+  let alt = 'CAT';
+  cat.innerHTML = `
+  <img src = "${b.url}" alt=${alt}>
+  `;
+}
